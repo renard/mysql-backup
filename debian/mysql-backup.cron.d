2152390f48db197fed538140377b1aad0fe04a33
Sébastien Gross <sebchezwamorg>
2012-01-03 16:44:02 +0100

    Update cron.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- debian/mysql-backup.cron.d
+++ debian/mysql-backup.cron.d
@@ -2,5 +2,12 @@
 # Regular cron jobs for the mysql-backup package
 #
 
+# uncomment to prevent from sending mail (not recommended).
+# MAILTO=""
+
+PATH=/usr/sbin:/usr/bin:/sbin:/bin
+
 # to backup mysql.example.com just do something like:
 #0 4	* * *	mysql-backup [ -x /usr/bin/mysql-backup ] && /usr/bin/mysql-backup -c /etc/mysql-backup/mysql.example.com.cnf
+
+# 35 9 * * * root /usr/bin/mysql-backup snapshot; /usr/bin/mysql-backup dump
