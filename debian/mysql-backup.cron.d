b77bf9295c9079f66de28669f2faf83498b93bcf
Sébastien Gross <sebchezwamorg>
2014-03-07 12:32:03 +0100

    Update cron file.

--- debian/mysql-backup.cron.d
+++ debian/mysql-backup.cron.d
@@ -8,6 +8,6 @@
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
 # to backup mysql.example.com just do something like:
-#0 4	* * *	mysql-backup [ -x /usr/bin/mysql-backup ] && /usr/bin/mysql-backup -c /etc/mysql-backup/mysql.example.com.cnf
+#0 4	* * *	mysql-backup [ -x /usr/bin/mysql-backup ] && /usr/bin/mysql-backup -c /etc/mysql-backup/mysql.example.com.cnf --dump
 
-# 35 9 * * * root /usr/bin/mysql-backup snapshot; /usr/bin/mysql-backup dump
+# 35 9 * * * root /usr/bin/mysql-backup --snapshot --dump
